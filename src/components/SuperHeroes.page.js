5892f2109dd3d03993485f6a5c73c3a0a33c2052
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-08-10 18:03:15 -0700

    fetch and display the dummy data using axios (traditional method)

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export const SuperHeroesPage = () => {
-  return <div>Super heroes page</div>;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Axios Super Heroes Page</h2>
+      {data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 };
