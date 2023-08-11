e939696f723c4542998a40a647c5058cb89c76b9
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-08-10 21:28:38 -0700

    fetch and display dummy data using useQuery hook

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,6 +1,25 @@
 import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
 
+const fetchData = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 
 export const RQSuperHeroesPage = () => {
-  return <div>RQ super heroes page</div>;
+  // isLoading, data - same things as in the SuperHeroesPage component
+  const { isLoading, data } = useQuery("super-heroes", fetchData);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 };
